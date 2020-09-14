2856e72a9f61d9b11fc69ac4d7692aaac67be065
Lewis <mc.eunyul@gmail.com>
2020-09-14 12:26:03 +0900

    Appended license to all files.

--- src/main/kotlin/net/pengtul/pengcord/bot/botcmd/CommandHelper.java
+++ src/main/kotlin/net/pengtul/pengcord/bot/botcmd/CommandHelper.java
@@ -6,6 +6,26 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
+/*
+*    A message that delets after send
+*    Copyright (C) 2020  Lewis Rho
+*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+
 public class CommandHelper {
     public static void deleteAfterSend(String message, int duration, @NotNull final Message msg){
         msg.getChannel()
